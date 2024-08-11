d0fa310505b91da0bd988f3d12357c55f1423de1
Vinicius Dantas Santos <90792498+vncin@users.noreply.github.com>
2024-08-11 20:24:23 -0300

    Add files via upload

---
+++ organo-react-first/src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css';
+
+export const Banner = () => {
+    //JSX
+    return(
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal da página Organo" />
+        </header>
+    )
+
+}
+
